461a5ecc4ec0b86711792813a4506ba1e29b8099
Filip Chalupa <chalupa.filip@gmail.com>
2023-01-18 11:40:44 +0100

    Change rollup config to typescript

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,7 +2,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import typescript from 'rollup-plugin-typescript2'
-import packageJson from './package.json'
+import packageJson from './package.json' assert { type: 'json' }
 
 export default {
 	input: './src/index.ts',
