ba90d575d490b3475b3040f9d72d2ea0c18033b0
Filip Chalupa <chalupa.filip@gmail.com>
2025-08-06 18:00:14 +0200

    Allow to change storage on fast fingers

--- demo/components/FastFingers.tsx
+++ demo/components/FastFingers.tsx
@@ -1,7 +1,10 @@
-import { useMemo, useRef, type FunctionComponent } from 'react'
+import { useMemo, useRef, useState, type FunctionComponent } from 'react'
 import { useStorageBackedState } from '../../src'
 
 export const FastFingers: FunctionComponent = () => {
+	const [storage, setStorage] = useState<
+		'localStorage' | 'sessionStorage' | 'none'
+	>('localStorage')
 	const [keyStrokes, setKeyStrokes] = useStorageBackedState<null | {
 		text: string
 		firstStrokeTimestamp: number
@@ -9,6 +12,12 @@ export const FastFingers: FunctionComponent = () => {
 	}>({
 		key: 'fast-fingers',
 		defaultValue: null,
+		storage:
+			storage === 'localStorage'
+				? localStorage
+				: storage === 'sessionStorage'
+					? sessionStorage
+					: null,
 	})
 
 	const characterPerMinute = useMemo(() => {
@@ -52,6 +61,18 @@ export const FastFingers: FunctionComponent = () => {
 					})
 				}}
 			/>
+			<select
+				value={storage}
+				onChange={(event) => {
+					setStorage(
+						event.target.value as 'localStorage' | 'sessionStorage' | 'none',
+					)
+				}}
+			>
+				<option value="localStorage">localStorage</option>
+				<option value="sessionStorage">sessionStorage</option>
+				<option value="none">no storage</option>
+			</select>{' '}
 			<button
 				onClick={() => {
 					setKeyStrokes(null)
