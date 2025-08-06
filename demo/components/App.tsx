26af4922e53a6399d7134a61eab37cf18f35798c
Filip Chalupa <chalupa.filip@gmail.com>
2025-08-06 13:22:20 +0200

    Demo: add more complex counter

--- demo/components/App.tsx
+++ demo/components/App.tsx
@@ -1,28 +1,11 @@
 import type { FunctionComponent } from 'react'
-import { useStorageBackedState } from '../../src'
+import { Counter } from './Counter'
 
 export const App: FunctionComponent = () => {
-	const [count, setCount] = useStorageBackedState(0, 'count')
-
 	return (
-		<section>
-			<h1>
-				Value: <output>{count}</output>
-			</h1>
-			<button
-				onClick={() => {
-					setCount(count + 1)
-				}}
-			>
-				increment
-			</button>
-			<button
-				onClick={() => {
-					setCount(count - 1)
-				}}
-			>
-				decrement
-			</button>
-		</section>
+		<>
+			<h1>useStorageBackedState</h1>
+			<Counter />
+		</>
 	)
 }
---
+++ demo/components/Counter.tsx
@@ -0,0 +1,36 @@
+import type { FunctionComponent } from 'react'
+import { useStorageBackedState } from '../../src'
+
+export const Counter: FunctionComponent = () => {
+	const [count, setCount] = useStorageBackedState(0, 'count')
+
+	return (
+		<section>
+			<h2>Counter</h2>
+			<p>
+				Value: <output>{count}</output>
+			</p>
+			<button
+				onClick={() => {
+					setCount(count + 1)
+				}}
+			>
+				increment ➕
+			</button>{' '}
+			<button
+				onClick={() => {
+					setCount(count - 1)
+				}}
+			>
+				decrement ➖
+			</button>{' '}
+			<button
+				onClick={() => {
+					window.location.reload()
+				}}
+			>
+				reload page 🔃
+			</button>
+		</section>
+	)
+}
