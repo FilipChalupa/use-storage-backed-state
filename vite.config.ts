5e1186dd33afabb6fa3356d8279b684e581f461b
Filip Chalupa <chalupa.filip@gmail.com>
2025-08-06 11:58:39 +0200

    Add demo base

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+	root: 'demo',
+	build: {
+		outDir: '../demo-dist',
+		emptyOutDir: true,
+	},
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'use-storage-backed-state': '../src/index',
+		},
+	},
+})
