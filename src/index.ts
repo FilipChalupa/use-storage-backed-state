dfba7c35af9cbde7034cc3ecaf037abd1160fb78
Filip Chalupa <chalupa.filip@gmail.com>
2025-08-06 16:25:21 +0200

    Add new implementation

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,70 @@
+import { useCallback, useMemo, useRef, useSyncExternalStore } from 'react'
 type Key = string
 type Storage = globalThis.Storage | null
 const defaultStorage = 'localStorage' in globalThis ? localStorage : null
 
 // @TODO: make default value optional
 
+const runtimeStorage = new Map<Key, string>()
+
 export const defaultParse = <Value>(value: string) => {
+	if (value === 'undefined') {
+		return undefined as Value
+	}
 	return JSON.parse(value) as Value
 }
 export const defaultStringify = <Value>(value: Value) => {
 	return JSON.stringify(value)
+}
+
+const runtimeBroadcastChannels = new Map<
+	Storage,
+	Map<Key, Set<(value: string | null) => void>>
+>()
+
+const broadcastRuntimeNewValue = (
+	key: Key,
+	value: string | null,
+	storage: Storage,
+) => {
+	runtimeBroadcastChannels
+		.get(storage)
+		?.get(key)
+		?.forEach((callback) => {
+			callback(value)
+		})
+}
+
+const subscribeNewValue = (
+	key: Key,
+	onChange: (value: string | null) => void,
+	storage: Storage,
+) => {
+	const handleStorageChange = (event: StorageEvent) => {
+		if (event.storageArea !== storage || event.key !== key) {
+			return
+		}
+		onChange(event.newValue)
+	}
+	if (storage !== null) {
+		window.addEventListener('storage', handleStorageChange)
+	}
+	const byStorageChannels =
+		runtimeBroadcastChannels.get(storage) ??
+		new Map<Key, Set<(value: string | null) => void>>()
+	runtimeBroadcastChannels.set(storage, byStorageChannels)
+	const byKeyChannels =
+		byStorageChannels.get(key) ?? new Set<(value: string | null) => void>()
+	byStorageChannels.set(key, byKeyChannels)
+	byKeyChannels.add(onChange)
+
+	const unsubscribe = () => {
+		if (storage !== null) {
+			window.removeEventListener('storage', handleStorageChange)
+			runtimeBroadcastChannels.get(storage)?.get(key)?.delete(onChange)
+		}
+	}
+	return { unsubscribe }
 }
 
 export const setStorageBackedValue = <Value>({
@@ -22,23 +78,47 @@ export const setStorageBackedValue = <Value>({
 	storage?: Storage
 	stringify?: (value: Value) => string
 }) => {
-	// @TODO
+	const stringifiedValue = stringify(value)
+	if (storage === null) {
+		runtimeStorage.set(key, stringifiedValue)
+	} else {
+		storage.setItem(key, stringifiedValue)
+	}
+	broadcastRuntimeNewValue(key, stringifiedValue, storage)
 }
 
-export const getStorageBackedValue = <Value>({
-	key,
-	defaultValue,
-	storage = defaultStorage,
-	parse = defaultParse,
-}: {
-	key: Key
-	defaultValue: Value
-	storage?: Storage
-	parse?: (value: string) => Value
-}): Value => {
-	// @TODO
-	return defaultValue
-}
+export const getStorageBackedValue = (() => {
+	const alreadyWarnedAboutMalformedData = new Map<Key, string>()
+	return <Value>({
+		key,
+		defaultValue,
+		storage = defaultStorage,
+		parse = defaultParse,
+	}: {
+		key: Key
+		defaultValue: Value
+		storage?: Storage
+		parse?: (value: string) => Value
+	}): Value => {
+		const rawValue = storage
+			? storage.getItem(key)
+			: (runtimeStorage.get(key) ?? null)
+		if (rawValue === null) {
+			return defaultValue
+		}
+		try {
+			return parse(rawValue)
+		} catch (error) {
+			if (alreadyWarnedAboutMalformedData.get(key) !== rawValue) {
+				alreadyWarnedAboutMalformedData.set(key, rawValue)
+				console.error(
+					`Malformed storage data for key ${key}. Falling back to defaultValue. Malformed data: ${rawValue}`,
+				)
+			}
+			return defaultValue
+		}
+	}
+})()
 
 export const removeStorageBackedValue = ({
 	key,
@@ -47,7 +127,12 @@ export const removeStorageBackedValue = ({
 	key: Key
 	storage?: Storage
 }) => {
-	// @TODO
+	if (storage === null) {
+		runtimeStorage.delete(key)
+	} else {
+		storage.removeItem(key)
+	}
+	broadcastRuntimeNewValue(key, null, storage)
 }
 
 export const subscribeStorageBackedValue = <Value>({
@@ -55,16 +140,25 @@ export const subscribeStorageBackedValue = <Value>({
 	defaultValue,
 	storage = defaultStorage,
 	parse = defaultParse,
+	onChange,
 }: {
 	key: Key
 	defaultValue: Value
 	storage?: Storage
 	parse?: (value: string) => Value
+	onChange: (value: Value) => void
 }) => {
-	// @TODO
-	const unsubscribe = () => {
-		// @TODO
+	const onInternalChange = () => {
+		onChange(
+			getStorageBackedValue({
+				key,
+				defaultValue,
+				storage,
+				parse,
+			}),
+		)
 	}
+	const { unsubscribe } = subscribeNewValue(key, onInternalChange, storage)
 	return { unsubscribe }
 }
 
@@ -76,12 +170,60 @@ export const useStorageBackedState = <Value>({
 	stringify = defaultStringify,
 }: {
 	key: Key
-	defaultValue: Value
+	defaultValue: Value | (() => Value)
 	storage?: Storage
 	parse?: (value: string) => Value
 	stringify?: (value: Value) => string
 }) => {
-	// @TODO
+	const evaluatedInitialValue = useMemo(
+		() =>
+			defaultValue instanceof Function
+				? (defaultValue as () => Value)()
+				: defaultValue,
+		[defaultValue],
+	)
+	const subscribe = useCallback(
+		(onStoreChange: () => void) =>
+			subscribeStorageBackedValue({
+				key,
+				defaultValue: evaluatedInitialValue,
+				storage,
+				parse,
+				onChange: onStoreChange,
+			}).unsubscribe,
+		[],
+	)
+	const getSnapshot = useCallback(
+		() =>
+			getStorageBackedValue({
+				key,
+				defaultValue: evaluatedInitialValue,
+				storage,
+				parse,
+			}),
+		[],
+	)
+	const getServerSnapshot = useCallback(() => evaluatedInitialValue, [])
+
+	const value = useSyncExternalStore<Value>(
+		subscribe,
+		getSnapshot,
+		getServerSnapshot,
+	)
+
+	const valueRef = useRef(value)
+	valueRef.current = value
+	const setValue = (newValue: Value | ((oldValue: Value) => Value)) => {
+		setStorageBackedValue({
+			key,
+			value:
+				newValue instanceof Function ? newValue(valueRef.current) : newValue,
+			storage,
+			stringify,
+		})
+	}
+
+	return [value, setValue] as const
 }
 
 export const storageBackedState = <Value>({
@@ -104,7 +246,13 @@ export const storageBackedState = <Value>({
 			setStorageBackedValue({ key, storage, value, stringify }),
 		get: () => getStorageBackedValue({ key, storage, defaultValue, parse }),
 		remove: () => removeStorageBackedValue({ key, storage }),
-		subscribe: () =>
-			subscribeStorageBackedValue({ key, storage, defaultValue, parse }),
+		subscribe: (onChange: (value: Value) => void) =>
+			subscribeStorageBackedValue({
+				key,
+				storage,
+				defaultValue,
+				parse,
+				onChange,
+			}),
 	}
 }
