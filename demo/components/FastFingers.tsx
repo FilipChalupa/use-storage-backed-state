42b286987ad52d325fa791765265176640b88e66
Filip Chalupa <chalupa.filip@gmail.com>
2025-08-07 14:02:42 +0200

    Fast fingers: change key on the fly

--- demo/components/FastFingers.tsx
+++ demo/components/FastFingers.tsx
@@ -2,6 +2,9 @@ import { type FunctionComponent, useMemo, useRef, useState } from 'react'
 import { useStorageBackedState } from '../../src'
 
 export const FastFingers: FunctionComponent = () => {
+	const [key, setKey] = useState<
+		'fast-fingers' | 'slow-stubs' | 'lazy-pinkies'
+	>('fast-fingers')
 	const [storage, setStorage] = useState<
 		'localStorage' | 'sessionStorage' | 'none'
 	>('localStorage')
@@ -10,7 +13,7 @@ export const FastFingers: FunctionComponent = () => {
 		firstStrokeTimestamp: number
 		lastStrokeTimestamp: number
 	}>({
-		key: 'fast-fingers',
+		key,
 		defaultValue: null,
 		storage:
 			storage === 'localStorage'
@@ -41,6 +44,10 @@ export const FastFingers: FunctionComponent = () => {
 		<section>
 			<h2>Fast fingers</h2>
 			<p>
+				Example with <code>key</code> and <code>storage</code> changed on the
+				fly.
+			</p>
+			<p>
 				Characters per minute:{' '}
 				<output>{characterPerMinute ?? 'start typing'}</output>
 			</p>
@@ -61,18 +68,39 @@ export const FastFingers: FunctionComponent = () => {
 					})
 				}}
 			/>
-			<select
-				value={storage}
-				onChange={(event) => {
-					setStorage(
-						event.target.value as 'localStorage' | 'sessionStorage' | 'none',
-					)
-				}}
-			>
-				<option value="localStorage">localStorage</option>
-				<option value="sessionStorage">sessionStorage</option>
-				<option value="none">no storage</option>
-			</select>{' '}
+			<label>
+				Storage:{' '}
+				<select
+					value={storage}
+					onChange={(event) => {
+						setStorage(
+							event.target.value as 'localStorage' | 'sessionStorage' | 'none',
+						)
+					}}
+				>
+					<option value="localStorage">localStorage</option>
+					<option value="sessionStorage">sessionStorage</option>
+					<option value="none">no storage</option>
+				</select>
+			</label>{' '}
+			<label>
+				Key:{' '}
+				<select
+					value={key}
+					onChange={(event) => {
+						setKey(
+							event.target.value as
+								| 'fast-fingers'
+								| 'slow-stubs'
+								| 'lazy-pinkies',
+						)
+					}}
+				>
+					<option value="fast-fingers">fast-fingers</option>
+					<option value="slow-stubs">slow-stubs</option>
+					<option value="lazy-pinkies">lazy-pinkies</option>
+				</select>
+			</label>{' '}
 			<button
 				type="button"
 				onClick={() => {
